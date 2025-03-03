40af358037905005e12ba55a2ca62ee46ca45381
Usama1190 <usamaisrar1190@gmail.com>
2025-03-03 13:41:04 +0500

    Class 2nd code which is data types in python on mar 03, 25

---
+++ py2-types-030325/README.md
@@ -0,0 +1,78 @@
+cursor
+binary type
+
+byte_data : byte = b"usama"
+
+binary mode
+
+with open("example.png", "rb") as image:
+    image_data = image.read()
+    print image
+
+
+Type casting
+
+pylance
+
+git clone url
+
+git commands
+git add .
+git commit -m ""
+git status
+git push
+
+
+operators
+
++     addition
+-     substraction
+*     multiplication
+/     devision
+%     modular
+//    floor division
+**    exponential        2 ** 3 = 8           2 * 2 * 2
+
+
+logical operator
+
+not
+bitwise not
+
+
+
+comparison operator
+
+==           ismai === ka concept nahi hota
+>=
+<=
+>
+<
+!=
+not
+
+
+
+
+
+and     True and True and False
+or     True or True or False
+
+
+
+
+assignment operator
+=
++=
+-=
+
+
+devision return karta h float
+
+
+x /= y
+x %= y
+x //= y
+
+
+
---
+++ py2-types-030325/class2.py
@@ -0,0 +1,110 @@
+# text : str = 'hello world'
+# number : int = 10
+# decimal_value : float = 10.5
+# boolean : bool = True
+# mutable_collection_of_data : list[int] = [1, 2, 3, 4, 5]
+# immutable_collection_of_data : tuple[int, str, float] = [1, 'hello', 3.14]
+# object_data : dict[str, [str, int]] = {"name" : "usama", "father name" : "israr"}
+# person_data : list[str] = ["usama", "israr", "20"]
+# unique_data : set[int] = {1, 1 ,12, 33, 45, 55, 2, 3, 3}
+# # immutable_unique_values : FrozenSet[int] = {1, 2, 3, 4, 34, 2, 4,5, 5, 67 ,78, 78, 45, 45, 56}
+# number_list : list[int] = range(1, 20)
+
+# for num in range(1, 20):
+    # print(num)
+
+# print(number_list)
+
+# print(chr(90))
+
+# a = 6
+# print(id(a))
+
+# b = a
+
+# b = 7
+# print(id(b))
+
+# Type casting
+
+# c = 8
+# print(c)
+# print(type(c))
+
+# c = str(c)
+# print(type(c))
+
+
+
+
+# c : int = 8
+# if type(c) == int:
+#     print('c is an integer')
+# else:
+#     print('c is not an integer')
+
+
+# c : int = 8
+# print(isinstance(c, int))
+# print(isinstance(c, float))
+
+
+
+
+
+# operator and operand
+
+# logical operator
+
+# a = True
+
+# print(not a)
+
+
+
+# Identity Operator
+
+# a = True
+# print(a is True)
+# print(a is not True)
+
+
+
+# Membership Operator
+
+# names: list[str] = ['usama', 'ali', 'asad']
+
+# result : bool = 'usama' in names;
+# result1 : bool = 'usama' not in names
+
+# print(result)
+# print(result1)
+
+# case sensitive , indentation
+
+
+
+
+# keywords
+
+# del result1
+# print(result1)
+
+
+
+# 39 keywords in python
+
+
+# naming convension in python
+
+
+
+
+# assigning different values
+
+# ziping method        in this method we do not asign types
+x, y, z = 1, 2.5, 'python'      
+
+# lesson 3 completed
+# assignment 
+# python operators 7 types
