0140a870d39d2621cdc974148c55f5834ce32804
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2025-03-20 03:15:51 +0500

    some explore about types in python which is str, int, float, bool, list, and tuple

--- piaic/py1_hello_string/class1.py
+++ piaic/py1_hello_string/class1.py
@@ -1,4 +1,33 @@
-print('hello usama')
+# name:any = 'Pakistan'
+# print(name)  # print
+# print(type(name))  # type
+# print(id(name))  # physical address
+# print(dir(name))
 
-print('Pakistan')
-print('Zinda bad')
+
+# name:str = 700
+# print(name)
+
+
+# name:str = 'Usama Israr'
+# print(name)
+
+
+# name = 'Usama Israr'
+# name = 700
+# print(name)  # print
+# print(type(name))  # type
+# print(id(name))  # physical address
+
+
+# name = 'Usama Isra'
+# print(name)
+# print(type(name))
+# print(id(name))
+
+
+# print('Pakistan')
+# print('Zinda bad')
+
+
+# print('hello usama')
