28381cbd88831abf57ebfd1a300e944506325077
Usama Israr <156895405+Usama1190@users.noreply.github.com>
2025-05-28 02:47:30 +0500

    Class one completed of modern python which is installation, setup, intro, and explore types

---
+++ py01/class1.py
@@ -0,0 +1,143 @@
+# print("Hello world! Usama here I am a student of quater 4 from Karachi")
+
+#-----
+
+# print("Usama Israr!")
+
+#-----
+
+# print("Usama Israr Khan!")
+
+# print("Osama Israr Khan!")
+
+# print("AI Developer")
+
+# print("Data Scientics")
+
+#-----
+
+# print("Pakistan")
+
+#-----
+
+# print("pakistan zindabad")
+
+#-----
+
+# print("Zinda bad")
+
+#-----
+
+# print("Zinda bad")
+# print("Zinda bad")
+# print("Zinda bad")
+# print("Zinda bad")
+# print("Zinda bad")
+# print("Zinda bad")
+# print("Zinda bad")
+
+#-----
+
+# name = "Osama Israr Khan!"
+
+# print(name)     # print Osama Israr Khan
+
+#-----
+
+# name: str = "Usama Israr Khan!"              # restrict data type
+
+# print(name)   # print Usama Israr Khan
+
+#-----
+
+# name: str = "Usama Israr Khan"
+
+# print(name)                     # print
+# print(type(name))               # type
+# print(id(name))
+# print(dir(name))                # methods and attributes
+
+#-----
+
+# name: str = "Usama Israr Khan"
+
+# print(name)                                        # print
+# print(type(name))                                  # type
+# print(id(name))                                    # physical address
+# print([i for i in dir(name) if "__" not in i])     # methods and attributes
+
+#-----
+
+# name: int = 700
+
+# print(name)                                         # print
+# print(type(name))                                   # type
+# print(id(name))                                     # physical address
+# print([i for i in dir(name) if "__" not in i])      # methods and attributes
+
+#-----
+
+# name: float = 7.0
+
+# print(name)                                         # print
+# print(type(name))                                   # type
+# print(id(name))                                     # physical address
+# print([i for i in dir(name) if "__" not in i])      # methods and attributes
+
+#-----
+
+# name: bool = True
+
+# print(name)                                         # print
+# print(type(name))                                   # type
+# print(id(name))                                     # physical address
+# print([i for i in dir(name) if "__" not in i])      # methods and attributes
+
+#-----
+
+# name: list[str] = ["a", "b", "c"]
+
+# print(name)                                         # print
+# print(type(name))                                   # type
+# print(id(name))                                     # physical address
+# print([i for i in dir(name) if "__" not in i])      # methods and attributes
+
+#-----
+
+# name: list[str] = ["a", "b", "c", 200]
+
+# print(name)                                            # print
+# print(type(name))                                      # type
+# print(id(name))                                        # physical address
+# print([i for i in dir(name) if "__" not in i])         # methods and attributes
+
+#-----
+
+# name: tuple[str, int, float] = ("pakistan", 200, 2.0)
+
+# print(name)                                            # print
+# print(type(name))                                      # type
+# print(id(name))                                        # physical address
+# print([i for i in dir(name) if "__" not in i])         # methods and attributes
+
+#-----
+
+# name: tuple[str, int, float] = ("pakistan", 200, 'abc')
+
+# print(name)                                           # print
+# print(type(name))                                     # type
+# print(id(name))                                       # physical address
+# print([i for i in dir(name) if "__" not in i])        # methods and attributes
+
+#-----
+
+# name: any = "pakistan"
+
+# print(name)                                           # print
+# print(type(name))                                     # type
+# print(id(name))                                       # physical address
+# print([i for i in dir(name) if "__" not in i])        # methods and attributes
+
+#-----
+
+#----- THE END -----
